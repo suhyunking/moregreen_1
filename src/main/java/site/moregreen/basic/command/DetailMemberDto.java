1092dee808c2d5e0236da576718d352317af92f8
suhyunking <kksshh09211@gmail.com>
2022-10-18 23:30:53 +0900

    update

--- src/main/java/site/moregreen/basic/command/DetailMemberDto.java
+++ src/main/java/site/moregreen/basic/command/DetailMemberDto.java
@@ -1,5 +1,17 @@
 package site.moregreen.basic.command;
 
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DetailMemberDto {
 	private Integer m_num;
 	private String m_id;
